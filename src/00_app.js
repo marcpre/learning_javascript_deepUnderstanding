74c01aca706ac1d45a80928b7c39ae8f01577f5e
marcpre <marcus.presich@gmail.com>
2018-08-04 06:21:06 +0000

    hoisting

--- src/00_app.js
+++ src/00_app.js
@@ -1,7 +1,13 @@
 // the global this and window object is available in the browser
 
+
+b()
+console.log(a)
+
+// these variables and functions get attached to window!
 var a = "Hello World"
 
 function b(){
-    
+    console.log("Called b!")
 }
+
