6069050c0d3a485fc7d8761604dac90bd77c5cca
marcpre <marcus.presich@gmail.com>
2018-08-09 04:26:38 +0000

    existance

---
+++ src/09_app.js
@@ -0,0 +1,12 @@
+// console.log(Boolean(undefined))
+
+var a;
+
+// goes out to the internet and looks for a value
+
+a = 0
+
+if( a || a === 0) {
+ console.log("Sth is there...")   
+ console.log(a)
+}
