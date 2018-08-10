28a89f98087972cac3e8640640fd018b01bbabe5
marcpre <marcus.presich@gmail.com>
2018-08-10 05:49:37 +0000

    default values

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 [https://www.udemy.com/understand-javascript/](https://www.udemy.com/understand-javascript/)
 
---> 03/021
+--> 03/028
---
+++ src/10_app.js
@@ -0,0 +1,8 @@
+function greet(name) {
+ name =  name || '<Your name here>' // default value
+ console.log("Hello " + name)
+}
+
+
+greet("Tony")
+greet()
