a7a2a3929cf100869a5709d8d2edb32d9778d1c9
marcpre <marcus.presich@gmail.com>
2018-08-26 08:57:30 +0000

    initialization

---
+++ src/41_app.js
@@ -0,0 +1,24 @@
+var people = [
+    {
+    // john object
+        firstname: 'John',
+        lastname: 'Doe',
+        addresses: [
+            '111 Main Str',
+            '123 asdfas'
+        ]
+    },
+    {
+        firstname: 'Jane',
+        lastname: 'Doe',
+        addresses: [
+            '111 Main Str',
+            '123 asdfas'
+        ],
+        greet: function() {
+            return 'Hello';
+        }
+    }
+]
+
+console.log(people)
