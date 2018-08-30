276d45f000a5e4116cfdc8574c5a788af2e314e3
marcpre <marcus.presich@gmail.com>
2018-08-30 06:32:19 +0000

    global object

--- src/45_Greetr.js
+++ src/45_Greetr.js
@@ -1,4 +1,20 @@
-(function( global, factory ) {
+(function(global, $) {
 
+    var Greetr = function(firstName, lastName, language) {
+        return new Greetr.init(firstName, lastName, language)
+    }
+    
+    Greetr.prototype = {};
 
-}(window, jQuery))
+    Greetr.init = function(firstName, lastName, language) {
+        var self = this;
+        self.firstName = firstName || '';
+        self.lastName = lastName || '';
+        self.language = language || '';
+    }
+    
+    Greetr.init.prototype = Greetr.prototype;
+    
+    global.Greetr = global.G$ = Greetr;
+
+}(window, jQuery));
