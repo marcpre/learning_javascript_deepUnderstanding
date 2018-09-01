fc9fffe9982d40cf398513ace099b75f296a486b
marcpre <marcus.presich@gmail.com>
2018-09-01 04:19:00 +0000

    use library

--- src/45_app.js
+++ src/45_app.js
@@ -1,3 +1,15 @@
-var g = G$('John', 'Doe')
+// gets a new object
+var g = G$('John', 'Doe');
+
 g.greet().setLang('es').greet(true).log();
 
+$('#login').click(function() {
+    var loginGrtr = G$('John', 'Doe');
+
+    // hide the login on the screen
+    $('#logindiv').hide();
+
+    // set language and log to console
+    loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greetings', true).log();
+
+});
